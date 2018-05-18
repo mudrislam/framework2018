6fef74c15c71e921a79c6ff7d11d009f067b7d9b
muhammed r islam <mudrislam@gmail.com>
2018-05-18 16:28:06 -0400

    fisrt framework

---
+++ src/base/config.java
@@ -0,0 +1,25 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class config {
+	//System.setProperty("webdriver.gecko.driver", "/Users/muhammedrislam/selenium_jar_files/geckodriver");
+	WebDriver driver = new FirefoxDriver();
+	//driver.manage().window().fullscreen();
+	//driver.get("http:www.facebook.com");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}	
+	
+	
+
